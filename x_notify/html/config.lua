74049d30a48b0770bf47709c80308640c513bd75
xad420 <98492604+xad420@users.noreply.github.com>
2023-03-18 11:25:49 +0100

    Delete config.lua

--- x_notify/html/config.lua
+++
@@ -1,4 +0,0 @@
-config = {
-    img: 'https://cdn.tipo.live/files/avatar/6496_avatar.jpg', -- zmień link do obrazka na żądany
-    maxOpened: 5 -- zmień liczbę maksymalnie otwartych powiadomień na żądaną
-}
